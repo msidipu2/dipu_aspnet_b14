41bab4dca0e054b1eb2187ccde3579191c3cb8ed
msidipu2 <msidipu2@gmail.com>
2026-07-25 18:50:30 +0600

    Assignment1, solved 3 problem.

---
+++ Assignment1/CF_282_A/Program.cs
@@ -0,0 +1,21 @@
+﻿int  numberoOfinput= Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (numberoOfinput-- > 0)
+{
+    string value = Console.ReadLine();
+
+    if (value[1] == '+')
+    {
+        count++;
+    }
+    if (value[1] == '-')
+    {
+        count--;
+    }
+
+
+
+}
+
+Console.WriteLine(count);
+
---
+++ Assignment1/CF_71_A/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberOfWord = Convert.ToInt32(Console.ReadLine());
+
+while (numberOfWord > 0) { 
+    string word = Console.ReadLine();
+    int length = word.Length;
+    if (length <= 10)
+    {
+        Console.WriteLine(word);
+    }
+    else
+    {
+        Console.WriteLine(word[0] + (length - 2).ToString() + word[length - 1]);
+    }
+    numberOfWord--;
+}
